0e7eb925dd51b052b5da626418908ed680e5a276
chris dziedzic <cdziedzic@gmail.com>
2023-10-04 11:51:23 -0400

    merging

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -8,7 +8,7 @@ import {
   Row
 } from 'react-bootstrap';
 import { useMutation, useQuery } from '@apollo/client';
-import { SAVE_BOOK } from '../utils/mutations';
+// import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -21,7 +21,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-  const [saveBook, {data, error}] = useMutation(SAVE_BOOK);
+  // const [saveBook, {data, error}] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
