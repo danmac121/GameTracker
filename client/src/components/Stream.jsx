2a7c42737e58b7a9caa2740e670eca9562104501
chris dziedzic <cdziedzic@gmail.com>
2023-10-07 11:33:58 -0400

    added placeholder div with stream box

--- client/src/components/Stream.jsx
+++ client/src/components/Stream.jsx
@@ -4,26 +4,31 @@ import { useState } from "react";
 function Stream() {
     const [stream, setStream] = useState('');
     const [toggle, setToggle] = useState(false)
+    const divStyle = {
+        height: '720px',
+        width: '1280px',
+        backgroundColor: 'lightblue', // You can add more styles as needed
+      };
 
+    //toggle stream on/off on button click
     const startStream = () => {
         console.log(toggle)
         setToggle(!toggle)
+        setStream('')
     };
 
     return (
         <>
+            {/* while toggle is false hide the player and show a div  */}
+            {toggle ?   <iframe src={`https://player.kick.com/${stream}`} height="720" width="1280" frameborder="0" scrolling="no" allowfullscreen="true"></iframe> : <div style={divStyle}></div>}
+            <br />
             <input
                 type="text"
                 value={stream}
                 placeholder="enter your favorite streamer"
                 onChange={(e) => setStream(e.target.value)}
             />
-            {toggle ?   <iframe src={`https://player.kick.com/${stream}`} height="720" width="1280" frameborder="0" scrolling="no" allowfullscreen="true"></iframe> : <div className="bg-dark" height="720" width="1280"></div>}
-            <button onClick={() => startStream()}>Start Stream</button>
-
-           
-
-            
+            <button onClick={() => startStream()}>{toggle ? "Change Streamer" : "Start Stream"} </button>
         </>
     )
 
